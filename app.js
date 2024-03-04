a0b5fd3910b2bf61697458ccf3511f8ffaacc740
Elijah Prince <elijahprince73@gmail.com>
2024-03-03 18:55:08 -0800

    feat: Adding get tours route

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+const fs = require('fs');
 const express  = require('express');
+
 
 const app =express();
 
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.status(200).json({message: 'Hello from server', app: 'Natours'})
+// app.get('/', (req, res) => {
+//   res.status(200).json({message: 'Hello from server', app: 'Natours'})
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  })
 })
 
 app.post('/', (req, res) => {
