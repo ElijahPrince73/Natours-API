c68de553f91c7f01b3452579629a26120b83c99e
Elijah Prince <elijahprince73@gmail.com>
2024-03-03 19:13:46 -0800

    feat: Adding new tour

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
 const express  = require('express');
+const { error } = require('console');
 
 
 const app =express();
 
+// middleware to modify the request by adding the incomeing data to be on the req.body
+app.use(express.json())
+
 const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({message: 'Hello from server', app: 'Natours'})
-// })
+const toursFile = `${__dirname}/dev-data/data/tours-simple.json`
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`))
+const tours = JSON.parse(fs.readFileSync(toursFile))
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -22,8 +24,20 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
-app.post('/', (req, res) => {
-  res.send("You can also post here")
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours.length + 1;
+  const newTour = Object.assign({id: newId},req.body)
+
+  tours.push(newTour);
+
+  fs.writeFile(toursFile, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    })
+  })
 })
 
 app.listen(port, () => {
