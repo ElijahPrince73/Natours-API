5a80f360c7faf0661b59d1a8f9b4052d80f5f641
Elijah Prince <elijahprince73@gmail.com>
2024-03-04 15:51:19 -0800

    More routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express  = require('express');
 const { error } = require('console');
+const { json } = require('body-parser');
 
 
 const app =express();
@@ -24,6 +25,27 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  if(id > tours.length) {
+    return res.status(404).json({
+      status: '404',
+      message: "Invalid Id"
+    })
+  }
+
+  const tour = tours.find(el => el.id === id)
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour
+    }
+  })
+})
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours.length + 1;
   const newTour = Object.assign({id: newId},req.body)
@@ -37,6 +59,24 @@ app.post('/api/v1/tours', (req, res) => {
         tour: newTour
       }
     })
+  })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'UPDATED TOUR HERE'
+    }
+  })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'DELETED'
+    }
   })
 })
 
