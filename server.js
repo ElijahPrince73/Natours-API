431caeed04c5dcb418a8d68b500103cf896a9155
Elijah Prince <elijahprince73@gmail.com>
2024-03-16 18:23:23 -0700

    feat: scipts to start in dev or prod

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const dotenv = require('dotenv');
-const app  = require('./app')
 
 dotenv.config({
   path: './config.env'
 })
+
+const app  = require('./app')
 
 const port = process.env.PORT || 3000
 
