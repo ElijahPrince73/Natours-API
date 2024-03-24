7e57f4620a7575d64cd42e3e24111040aafd4665
Elijah Prince <elijahprince73@gmail.com>
2024-03-24 15:28:11 -0700

    feat: connecting to database

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const app = require("./app");
 
 dotenv.config({
   path: "./config.env",
 });
 
-const app = require("./app");
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => {
+  console.log("Database connected");
+});
 
 const port = process.env.PORT || 3000;
 
