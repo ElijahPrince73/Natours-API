a9c3756773f15718ed68d85a07a4829a3ba25f41
Elijah Prince <elijahprince73@gmail.com>
2024-03-24 17:10:46 -0700

    feat: saving documents to database

--- server.js
+++ server.js
@@ -16,6 +16,39 @@ mongoose.connect(DB).then(() => {
   console.log("Database connected");
 });
 
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", toursSchema);
+
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 4.97,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const port = process.env.PORT || 3000;
 
 // Server
