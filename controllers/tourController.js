8dee4236f24a1974f1835baaaa15dcc4a3bd7b9e
Elijah Prince <elijahprince73@gmail.com>
2024-04-17 14:50:02 -0700

    feat: delete a tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -84,11 +84,20 @@ exports.updatetour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "DELETED",
-    },
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
