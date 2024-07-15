883e01ad087b32c2e4f30086533b61deb5bba338
Elijah Prince <elijahprince73@gmail.com>
2024-07-15 16:04:29 -0700

    Refactor

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,11 +12,14 @@ exports.aliasTopTours = async (req, res, next) => {
   next();
 };
 
-exports.getAllTours = async (req, res) => {
-  try {
-    // BUILD THE QUERY
-    // 1 Filtering
-    const queryObj = { ...req.query };
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
     const excludedFieds = ["page", "sort", "limit", "fields"];
 
     excludedFieds.forEach((el) => delete queryObj[el]);
@@ -28,44 +31,61 @@ exports.getAllTours = async (req, res) => {
       (match) => `$${match}`,
     );
 
-    let query = Tour.find(JSON.parse(queryString));
+    this.query = this.query.find(JSON.parse(queryString));
 
-    // 3 Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.query.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
     } else {
-      query = query.sort("-createdAt");
+      this.query = this.query.sort("-createdAt");
     }
 
-    // 4 Field limiting
-    // Used for grabbing certain data
-    // EX: ?fields=name, duration, price, description
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
     } else {
-      query = query.select("-__v");
+      this.query = this.query.select("-__v");
     }
 
-    // 4) Pagination
+    return this;
+  }
 
-    const page = Number(req.query.page);
-    const limit = Number(req.query.limit) || 100;
+  paginate() {
+    const page = Number(this.queryString.page);
+    const limit = Number(this.queryString.limit) || 100;
     const skip = (page - 1) * limit;
     // EX: page=2,limit=10
-    query = query.skip(skip).limit(limit);
+    this.query = this.query.skip(skip).limit(limit);
 
-    if (req.query.page) {
-      const numOfTours = await Tour.countDocuments();
+    return this;
+  }
+}
 
-      if (skip >= numOfTours) {
-        throw new Error("This page does not exist");
-      }
-    }
+exports.getAllTours = async (req, res) => {
+  try {
+    // BUILD THE QUERY
+    // 1 Filtering
+    const queryObj = { ...req.query };
+    const excludedFieds = ["page", "sort", "limit", "fields"];
+
+    excludedFieds.forEach((el) => delete queryObj[el]);
 
     // EXECUTE THE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
