750b1876633190dde236bce63471c7b54520430e
Elijah Prince <elijahprince73@gmail.com>
2024-07-29 16:44:14 -0700

    Added 404 route

--- app.js
+++ app.js
@@ -16,4 +16,12 @@ app.use(express.static(`${__dirname}/public/`));
 app.use("/api/v1/tours", toursRouter);
 app.use("/api/v1/users", userRouter);
 
+// If we cant find the route
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: ` Can't find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
