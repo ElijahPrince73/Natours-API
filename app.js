1c53ff168bc7388327c2e32cda84bc09a0df395d
Elijah Prince <elijahprince73@gmail.com>
2024-08-01 16:00:06 -0700

    Global error handling

--- app.js
+++ app.js
@@ -18,9 +18,23 @@ app.use("/api/v1/users", userRouter);
 
 // If we cant find the route
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: ` Can't find ${req.originalUrl} on this server`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  // Pass the newly created error to the next middleware
+  // Express will always handle the error if given an argument in the next() function
+  next(err);
+});
+
+// Global middleware to handle error responses
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
