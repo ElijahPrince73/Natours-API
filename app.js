467def34756f252999ad71b084ef48a0f39aef85
Elijah Prince <elijahprince73@gmail.com>
2024-08-01 16:17:17 -0700

    Better errors and refactor

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError");
+
 const toursRouter = require("./routes/toursRoutes");
 const userRouter = require("./routes/userRoutes");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
@@ -18,24 +21,12 @@ app.use("/api/v1/users", userRouter);
 
 // If we cant find the route
 app.all("*", (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = "fail";
-  err.statusCode = 404;
-
   // Pass the newly created error to the next middleware
   // Express will always handle the error if given an argument in the next() function
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 // Global middleware to handle error responses
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,10 @@
+// Global middleware to handle error responses
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
