c448d7cbc5bde4ac7378e4d4a8673f72d5356f4d
Elijah Prince <elijahprince73@gmail.com>
2025-02-17 13:33:54 -0800

    Sign up new user

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+// Description: This file contains the functions to handle the user authentication.
+const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.signup = catchAsync(async (req, res) => {
+  const newUser = await User.create(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: newUser,
+    },
+  });
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,11 +6,24 @@ const {
   updateUser,
   deleteUser,
 } = require("../controllers/userController");
+const { signup } = require("../controllers/authController");
 
 const router = express.Router();
 
+// POST request to handle user signup
+// This route is used to create a new user account.
+// The signup function will handle the logic for registering a new user.
+router.post("/signup", signup);
+
+// Using router.route to handle multiple requests on the same route
+// This route handles GET and POST requests for the root URL ("/").
+// GET request to fetch all users (getAllUsers function).
+// POST request to create a new user (createUser function).
 router.route("/").get(getAllUsers).post(createUser);
 
+// GET request to fetch a specific user by ID (getUser function).
+// PATCH request to update a specific user by ID (updateUser function).
+// DELETE request to delete a specific user by ID (deleteUser function).
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
