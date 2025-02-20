5d96e1562e1ff4cc54449a4b0397faedb5b4e797
Elijah Prince <elijahprince73@gmail.com>
2025-02-19 16:38:32 -0800

    Add JWT authentication to user signup and update environment configuration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,23 @@
 // Description: This file contains the functions to handle the user authentication.
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const jwt = require("jsonwebtoken");
 
 exports.signup = catchAsync(async (req, res) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
