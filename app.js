7d8662bc2abdca3292b5b1173e43a5a926bb0fbf
Elijah Prince <elijahprince73@gmail.com>
2025-03-12 16:26:35 -0700

    rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
 const AppError = require("./utils/appError");
 
@@ -9,10 +10,20 @@ const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
-// Middleware
+// GLOBAL Middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Within an hour specify how many times the server can be accessed from an IP address.
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP, please try again in an hour",
+});
+
+app.use("/api", limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public/`));
 
